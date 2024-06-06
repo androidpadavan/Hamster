2b9284d2139c0fff68e36854b80122970da33711
ALRT <lopanov.arseniy@outlook.com>
2024-06-06 17:34:08 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,100 @@
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Practicum {
+
+    public static void main(String[] args) {
+        HamsterFactory hamsterFactory = new HamsterFactory();
+        hamsterFactory.start();
+    }
+}
+
+class HamsterFactory {
+    Scanner scanner = new Scanner(System.in);
+    ArrayList<Hamster> hamsters = new ArrayList<>();
+
+    public void start() {
+        System.out.println("Добро пожаловать на фабрику хомяков!");
+        System.out.println("Здесь вы можете создавать, удалять и даже заменять одних хомяков на других");
+        System.out.println("Для работы с фабрикой используйте следующие команды:");
+        System.out.println("'Печать' — показать всех активных хомяков");
+        System.out.println("'Создать [Имя]' — создать хомяка с выбранным именем");
+        System.out.println("'Удалить [Индекс]' — удалить хомяка по выбранному индексу");
+        System.out.println("'Очистить' — удалить всех, ранее созданных хомяков");
+        System.out.println("'Заменить [Индекс] [Имя]' — заменить хомяка под выбранным индексом на нового с выбранным именем");
+        System.out.println("'Размер' — вывести на экран текущее количество хомяков в хранилище");
+        System.out.println("'Завершить' — завершить работу программы");
+
+        while (true) {
+            System.out.println("Введите команду...");
+
+            String command = scanner.nextLine();
+
+            if (command.equals("Завершить")) {
+                System.out.println("Программа завершена! Спасибо, что пользуетесь нашей сетью хомячих фабрик");
+                break;
+            } else {
+                executeCommand(command);
+            }
+        }
+    }
+
+    private void executePrint() {
+        System.out.println("Вывожу актуальный список хомяков в хранилище:");
+        for (Hamster hamster : hamsters) {
+            System.out.println("Хомяк '" + hamster.name + "'");
+        }
+    }
+
+    private void executeCreate(String name) {
+        Hamster newHamster = new Hamster(name);
+        hamsters.add(newHamster);
+        System.out.println("Хомяк '" + name + "' создан и добавлен в хранилище");
+    }
+
+    private void executeRemoveByIndex(int index) {
+        if (index >= 0 && index < hamsters.size()) {
+            Hamster removedHamster = hamsters.remove(index);
+            System.out.println("Хомяк '" + removedHamster.name + "' удалён успешно");
+        } else {
+            System.out.println("Хомяка по заданному индексу не существует");
+        }
+    }
+
+    private void executeClear() {
+        hamsters.clear();
+        System.out.println("Операция очистки завершена успешно! Все хомяки были удалены из хранилища");
+    }
+
+    private void executeSet(int index, String name) {
+        if (index >= 0 && index < hamsters.size()) {
+            Hamster newHamster = new Hamster(name);
+            Hamster oldHamster = hamsters.set(index, newHamster);
+            System.out.println("Хомяк '" + oldHamster.name + "' был успешно заменён на хомяка '" + name + "'");
+        } else {
+            System.out.println("В хранилище нет хомяков");
+        }
+    }
+
+    private void executeSize() {
+        if (hamsters.size() > 0) {
+            System.out.println("Количество хомяков в хранилище равно " + hamsters.size());
+        } else {
+            System.out.println("В хранилище нет хомяков");
+        }
+    }
+
+    private void showErrorMessage() {
+        System.out.println("Неверная команда, попробуйте ещё раз.");
+    }
+}
+
+class Hamster {
+
+    String name;
+
+    public Hamster(String name) {
+        this.name = name;
+    }
+}
